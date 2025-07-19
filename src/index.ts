d43002ed57c4e6ae4cd0a8e6506be711df2f643d
Bananayosostene <sbananayo98@gmail.com>
2025-07-19 18:49:32 +0200

    remove js

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 // Swagger UI at /api-docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', MainRouter);
