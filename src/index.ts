d7d18954b2a570eb2e938e4755ff90bcdfb05107
IMANARIYO <imanariyobaptiste@gmail.com>
2025-07-21 00:07:16 +0200

    adding the   good documentation

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,31 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './docs/swagger.js';
 import MainRouter from './routes/allroutes.js';
 import passport from './utils/passport.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import session from 'express-session';
 import { globalErrorHandler, handleNotFoundRoutes } from './middlewares/errorHandler.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', MainRouter);
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/swagger.json', (_req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.get('/docs', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'swagger.html'));
+});
 app.use(handleNotFoundRoutes);
 app.use(globalErrorHandler);
 app.use(
@@ -28,9 +44,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+
 
 const PORT = Number(process.env.PORT) || 4000;
 app.listen(PORT, () => {
